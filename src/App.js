97e7b4840c1f3017c9aa41b1bdcd3ad7ea21892f
Edwar Jaramillo <eajs565@gmail.com>
2019-01-15 15:04:18 -0500

    Redux, react-redux
    Avance redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+//import MuiThemeProvider from '@material-ui/styles/MuiThemeProvider';
 import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
 import { Grid, Col, Row } from 'react-flexbox-grid';
+import PropTypes from 'prop-types';
 import './App.css';
 import LocationList from './components/LocationList';
 import ForecastExtended from './components/ForecastExtended';
+import { setCity } from './actions';
 
 const cities = [
   //'Pereira,col',
@@ -15,7 +19,7 @@ const cities = [
   //'Barranquilla,col',
   //'Bogota,col',
   //'Pasto,col',
-]
+];
 
 class App extends Component {
   constructor(){
@@ -26,41 +30,53 @@ class App extends Component {
   handleSelectionLocation = city => {
     this.setState({city: city});
     console.log(`handleSelectionLocation > (${city})`);
+    
+    this.props.dispatchSetCity(city);
   }
 
   render() {
     const { city } = this.state;
     return (
-      <Grid>
-        <Row>
-          <AppBar position='sticky'>
-            <Toolbar>
-              <Typography variant='title' color='inherit'>
-                Weahter App
-              </Typography>
-            </Toolbar>
-          </AppBar>
-        </Row>
-        <Row>
-          <Col xs={12} md={6} >
-            <LocationList cities={cities}
-            onSelectedLocation={this.handleSelectionLocation}/>
-          </Col>
-          <Col xs={12} md={6} >
-            <Paper elevation={4} >
-              <div className="details">
-                {
-                  !city ?
-                    null:
-                    <ForecastExtended city={city} />
-                }
-              </div>
-            </Paper>
-          </Col>
-        </Row>
-      </Grid>
+        <Grid>
+          <Row>
+            <AppBar position='sticky'>
+              <Toolbar>
+                <Typography variant='title' color='inherit'>
+                  Weahter App
+                </Typography>
+              </Toolbar>
+            </AppBar>
+          </Row>
+          <Row>
+            <Col xs={12} md={6} >
+              <LocationList cities={cities}
+              onSelectedLocation={this.handleSelectionLocation}/>
+            </Col>
+            <Col xs={12} md={6} >
+              <Paper elevation={4} >
+                <div className="details">
+                  {
+                    !city ?
+                      null:
+                      <ForecastExtended city={city} />
+                  }
+                </div>
+              </Paper>
+            </Col>
+          </Row>
+        </Grid>
     );
   }
 }
 
-export default App;
+//export default App;
+
+App.propTypes = {
+  setCity: PropTypes.func.isRequired,
+}
+
+const mapDispatchToPropsActions = dispatch => ({
+  dispatchSetCity: value => dispatch(setCity(value))
+});
+
+export default connect(null, mapDispatchToPropsActions)(App);
